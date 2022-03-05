ec4596137774542be54af3b609eb981f36900699
Thiago Pellegrin <tgpellegrin@gmail.com>
2022-03-05 10:06:54 -0300

    clean up Serverless Framework setup boilerplate

--- serverless.ts
+++ serverless.ts
@@ -1,7 +1,5 @@
 import type { AWS } from '@serverless/typescript';
 
-import hello from '@functions/hello';
-
 const serverlessConfiguration: AWS = {
   service: 'crm-service',
   frameworkVersion: '2',
@@ -24,8 +22,7 @@ const serverlessConfiguration: AWS = {
     },
     lambdaHashingVersion: '20201221',
   },
-  // import the function via paths
-  functions: { hello },
+  functions: {},
 };
 
 module.exports = serverlessConfiguration;
