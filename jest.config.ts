58d92e035bf821091c10abe972ccdc1d6383d292
Thiago Pellegrin <tgpellegrin@gmail.com>
2022-03-06 18:17:57 -0300

    test: set up Jest

---
+++ jest.config.ts
@@ -0,0 +1,18 @@
+module.exports = {
+  testTimeout: 15000,
+  verbose: true,
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+  },
+  moduleDirectories: [
+    'node_modules',
+    '.'
+  ],
+  moduleNameMapper: {
+    "@functions/(.*)": "src/functions/$1",
+    "@libs/(.*)": "src/libs/$1",
+    "@common/(.*)": "src/common/$1",
+    "@test/(.*)": "test/$1",
+    "@mocks/(.*)": "__mocks__/$1"
+  },
+};
