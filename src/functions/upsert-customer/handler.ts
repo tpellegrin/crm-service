c3b6c90017e60e9312cf263ea81a8ff349bc60da
Thiago Pellegrin <tgpellegrin@gmail.com>
2022-03-10 07:29:59 -0300

    feat: use S3 to save customer photo on upsert

--- src/functions/upsert-customer/handler.ts
+++ src/functions/upsert-customer/handler.ts
@@ -1,8 +1,12 @@
 import 'source-map-support/register';
 
+import { nanoid } from 'nanoid';
+
 import type { ValidatedEventAPIGatewayProxyEvent } from '@libs/apiGateway';
 import { formatJSONResponse } from '@libs/apiGateway';
 import { middyfy } from '@libs/lambda';
+import { getImageMetadata } from '@common/util';
+import s3 from '@common/s3';
 import dynamodb from '@common/dynamodb';
 
 import schema from './schema';
@@ -11,8 +15,23 @@ const handler: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (
   event
 ) => {
   try {
-    if (event.pathParameters?.id) {
-      await dynamodb.update(event.body, 'customer', event.pathParameters.id);
+    const id = event.pathParameters?.id || undefined;
+    const imageData = event.body.photo?.data || undefined;
+    const imageMime = event.body.photo?.mime || undefined;
+
+    if (imageData && imageMime) {
+      const image = await getImageMetadata(imageData);
+      const { URI: imageUrl } = await s3.save(
+        image.Data,
+        nanoid(10),
+        image.Mime,
+        image.Extension
+      );
+
+      event.body.photoUrl = imageUrl;
+    }
+    if (id) {
+      await dynamodb.update(event.body, 'customer', id);
       return formatJSONResponse({ message: 'customer updated' });
     } else {
       const id = await dynamodb.put(event.body, 'customer');
